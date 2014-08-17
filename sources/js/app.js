59c5d472c554ebbb69dba68d68105ab26a32ae86
Thoriq Firdaus <tfirdaus@creatiface.com>
2014-08-17 22:19:57 +0700

    Re-compile
    Missing the Bootstrap Dropdown

--- sources/js/app.js
+++ sources/js/app.js
@@ -1,4 +1,4 @@
 //@koala-prepend "../../components/jquery/dist/jquery.js"
 //@koala-prepend "../../components/bootstrap/js/transition.js"
-//@koala-prepend "../../components/bootstrap/js/dropdown"
+//@koala-prepend "../../components/bootstrap/js/dropdown.js"
 //@koala-prepend "../../components/jasny-bootstrap/js/offcanvas.js"
