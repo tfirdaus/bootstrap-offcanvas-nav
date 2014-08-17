f9f78c5242db2fa6e857ac44fcf8319bfb86a4f8
Thoriq Firdaus <tfirdaus@creatiface.com>
2014-08-17 21:28:18 +0700

    Add jQuery reference

--- sources/js/app.js
+++ sources/js/app.js
@@ -1,3 +1,4 @@
-//@codekit-prepend "../../components/bootstrap/js/transition.js"
-//@codekit-prepend "../../components/bootstrap/js/dropdown"
-//@codekit-prepend "../../components/jasny-bootstrap/js/offcanvas.js"
+//@koala-prepend "../../components/jquery/dist/jquery.js"
+//@koala-prepend "../../components/bootstrap/js/transition.js"
+//@koala-prepend "../../components/bootstrap/js/dropdown"
+//@koala-prepend "../../components/jasny-bootstrap/js/offcanvas.js"
