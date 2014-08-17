17d1eed40d809692fbdd1e189dcfa5e1f0129667
Thoriq Firdaus <tfirdaus@creatiface.com>
2014-08-17 19:35:45 +0700

    New files
    new folders and renaming

---
+++ sources/js/app.js
@@ -0,0 +1,3 @@
+//@codekit-prepend "../../components/bootstrap/js/transition.js"
+//@codekit-prepend "../../components/bootstrap/js/dropdown"
+//@codekit-prepend "../../components/jasny-bootstrap/js/offcanvas.js"
